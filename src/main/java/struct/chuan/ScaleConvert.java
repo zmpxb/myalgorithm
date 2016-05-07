c6e3e3798850ca76cf680fa3b3c289d92821ecfe
xiaozeli <747982714@qq.com>
2016-05-07 10:40:45 +0800

    二进制字符串转10进制添加一个sout

--- src/main/java/struct/chuan/ScaleConvert.java
+++ src/main/java/struct/chuan/ScaleConvert.java
@@ -28,6 +28,27 @@ public class ScaleConvert {
     }
 
     /**
+     * 二进制字符串转10进制
+     * @param str
+     * @return
+     */
+    public static int bitStringToDec(String str){
+        int result = 0;
+        int ch = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i)>='0'&&str.charAt(i)<='1'){
+                ch = str.charAt(i)-'0';
+            }else {
+                System.out.println("输入格式不合法");
+                return 0;
+            }
+            result = result*2 + ch;
+        }
+        System.out.println("hello");
+        return result;
+    }
+
+    /**
      * Created by xiaozl on 2016/3/2.
      * 16进制字符串转10进制整数
      * 输入："ab"
