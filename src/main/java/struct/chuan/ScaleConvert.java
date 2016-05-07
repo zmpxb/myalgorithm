2f8e4e39a39c25eb191e1c58c8cf1b84dda923eb
xiaozeli <747982714@qq.com>
2016-05-07 10:37:25 +0800

    二进制整数转10进制整数

--- src/main/java/struct/chuan/ScaleConvert.java
+++ src/main/java/struct/chuan/ScaleConvert.java
@@ -7,6 +7,26 @@ package struct.chuan;
  * @date: 2016/5/7
  */
 public class ScaleConvert {
+
+    /**
+     * 二进制整数转10进制整数
+     */
+    public static int bitToDec(int bit){
+        int result = 0;
+        int ch = 0;
+        String str = String.valueOf(bit);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i)>='0'&&str.charAt(i)<='1'){
+                ch = str.charAt(i)-'0';
+            }else {
+                System.out.println("输入格式不合法");
+                return 0;
+            }
+            result = result*2 + ch;
+        }
+        return result;
+    }
+
     /**
      * Created by xiaozl on 2016/3/2.
      * 16进制字符串转10进制整数
