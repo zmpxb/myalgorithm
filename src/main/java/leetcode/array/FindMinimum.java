878394cc0c4833899e135895243ddcc379608fd8
xiaozeli <747982714@qq.com>
2016-05-22 09:51:31 +0800

    求和、查找最小值

--- src/main/java/leetcode/array/FindMinimum.java
+++ src/main/java/leetcode/array/FindMinimum.java
@@ -9,8 +9,9 @@ package leetcode.array;
 public class FindMinimum {
 
     public static void main(String[] args) {
-        int [] nums = {0,1,2,3,4,5};
-        finMinInRotatedArray(nums);
+        int [] nums = {6,6,6,6,6,6,6,0,1,2,3,4,5};
+        int [] nums1 = {10,1,10,10,10};
+        System.out.println(finMinInRotatedArrayII(nums1));
         binarySearch(nums,4);
     }
 
@@ -73,6 +74,42 @@ public class FindMinimum {
             mid = (low + high) / 2;
         }
         return -1;
+    }
+
+    /**
+     * 这个题目的难点在于有重复元素：在遇到重复元素时我们选择的处理方法是start++，而不是start = mid，因为这样会漏掉元素
+     * @param nums
+     * @return
+     */
+    public static int finMinInRotatedArrayII(int [] nums){
+        int size = nums.length;
+        if (size == 0){
+            return 0;
+        } else if (size == 1){
+            return nums[0];
+        } else if (size == 2){
+            return Math.min(nums[0],nums[1]);
+        }
+
+        int low = 0;
+        int high = size-1;
+
+        while (low < high-1){
+            if (nums[low] < nums[high]){
+                return nums[low];
+            }
+            int mid = (high + low) / 2;
+            // 这里是问题关键
+            if (nums[mid] > nums[low]){
+                low = mid;
+            } else if (nums[mid]<nums[low]){
+                high = mid;
+            } else {
+                low++;
+            }
+        }
+        // 因为while中的条件是low < high-1
+        return Math.min(nums[low], nums[high]);
     }
 
 }
